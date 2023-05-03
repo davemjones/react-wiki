77be7b49570614843dd41f90f9be695d0ae17b83
David Jones <davemjones@gmail.com>
2023-05-03 06:52:31 +0000

    added environment for cert filename

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 dev_cert_secret=''
+dev_cert_filename=''
 
-while getopts 'p:' FLAG; do
+while getopts 'f:p:' FLAG; do
     case "${FLAG}" in 
       p) dev_cert_secret="${OPTARG}" ;;
+      f) dev_cert_filename="${OPTARG}" ;;
       *) exit 1;;
     esac
 done
 
+import_path="/home/vscode/.aspnet/https/"$dev_cert_filename
+
 (cd ClientApp && npm install)
-dotnet dev-certs https --clean --import /home/vscode/.aspnet/https/aspnetapp.pfx -p "$dev_cert_secret"
+dotnet dev-certs https --clean --import $import_path -p "$dev_cert_secret"
--- Readme.md
+++ Readme.md
@@ -1,2 +1,16 @@
 ## Template URL
 https://learn.microsoft.com/en-us/aspnet/core/client-side/spa/react?view=aspnetcore-7.0&tabs=netcore-cli
+
+## Secrets
+Create a secrets.env file in the .devcontainer folder.
+Contents should include at minimum the following:
+```
+DEV_CERT_PASSWORD=[your secure password]
+DEV_CERT_FILENAME=aspnetapp.pfx
+```
+
+## Create dev cert on host
+```
+dotnet dev-certs https -ep aspnetapp.pfx -p [your secure password]
+dotnet dev-certs https --trust
+```
